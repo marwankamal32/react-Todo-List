839be447cd0e849eed3b0b8df234a49fbb227995
Marwan <kamalmarwan855@gmail.com>
2025-04-23 17:55:52 +0200

    Implement task editing, deletion, and completion functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -10,7 +11,22 @@ import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined
 import IconButton from '@mui/material/IconButton';
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 
-export default function Todo(){
+//DIALOG
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+export default function Todo({title , details, isCompleted, onComplete, onDelete, onEdit}){
+
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+    const[openEditDialog, setOpenEditDialog ] = useState(false);
+    const[editTitle, setEditTitle] = useState("");
+    const[editDetails, setEditDetails] = useState("");
+
     return(
         <>
 
@@ -19,25 +35,58 @@ export default function Todo(){
 
                 <Grid container spacing={2}>
                     <Grid size={8}>
-                        <Typography variant = "h5" sx={{textAlign:"right"}}> المهمة الاولى</Typography>
-                        <Typography variant = "h6" sx={{textAlign:"right"}}> تفاصيل خاصة بالمهمة الاولى</Typography>
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            textAlign: "right",
+                            textDecoration: isCompleted ? "line-through" : "none",
+                            opacity: isCompleted ? 0.6 : 1,
+                        }}
+                        >
+                        {title}
+                        </Typography>
+
+                        <Typography
+                        variant="h6"
+                        sx={{
+                            textAlign: "right",
+                            textDecoration: isCompleted ? "line-through" : "none",
+                            opacity: isCompleted ? 0.6 : 1,
+                        }}
+                        >
+                        {details}
+                        </Typography>
                     </Grid>
 
                     {/*ACTION BUTTONS*/}
                     <Grid size={4} display="flex" justifyContent="space-around" alignItems="center">
 
-                        <IconButton 
-                        className="iconButton" aria-label="check" style={{color:"#49A950", background:"white",border:"solid #49A950 3px",}}>
+                        <IconButton
+                        className="iconButton" aria-label="check"
+                        onClick={onComplete}
+                        style={{
+                            color: isCompleted?"white":"#49A950", background: isCompleted?"#49A950":"white",border:"solid #49A950 3px",}}>
                             <CheckIcon />
                         </IconButton>
 
-                        <IconButton 
-                        className="iconButton" aria-label="edit" style={{color:"#808080", background:"white",border:"solid #808080 3px",}}>
+                        <IconButton
+                        className="iconButton" aria-label="edit"
+                        onClick={() => {
+                            setEditTitle(title);
+                            setEditDetails(details);
+                            setOpenEditDialog(true);
+                        }}
+                        style={{
+                            color:"#808080", background:"white",border:"solid #808080 3px",}}>
                             <ModeEditOutlineOutlinedIcon />
                         </IconButton>
 
-                        <IconButton 
-                        className="iconButton" aria-label="delete" style={{color:"#EC3432", background:"white",border:"solid #EC3432 3px",}}>
+                        <IconButton
+                        className="iconButton"
+                        aria-label="delete"
+                        onClick={() => setOpenDeleteDialog(true)}
+                        style={{
+                            color:"#EC3432", background:"white",border:"solid #EC3432 3px",}}>
                             <DeleteOutlineOutlinedIcon />
                         </IconButton>
 
@@ -46,6 +95,68 @@ export default function Todo(){
                 </Grid>
             </CardContent>
         </Card>
+        {/*=== DELETE DIALOG ===*/}
+        <Dialog open={openDeleteDialog} onClose={() => setOpenDeleteDialog(false)}>
+            <DialogTitle>{"تأكيد الحذف"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>هل أنت متأكد أنك تريد حذف هذه المهمة؟</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => setOpenDeleteDialog(false)}>لا</Button>
+                <Button
+                    onClick={() => {
+                        onDelete();
+                        setOpenDeleteDialog(false);
+                    }}
+                    color="error"
+                    autoFocus
+                >
+                    نعم
+                </Button>
+            </DialogActions>
+        </Dialog>
+        {/*=== END DELETE DIALOG ===*/}
+
+        {/*EDIT DIALOG */}
+        <Dialog open={openEditDialog} onClose={() => setOpenEditDialog(false)}>
+            <DialogTitle>تعديل المهمة</DialogTitle>
+            <DialogContent>
+                <TextField
+                autoFocus
+                margin="dense"
+                label="عنوان المهمة"
+                type="text"
+                fullWidth
+                value={editTitle}
+                onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <TextField
+                margin="dense"
+                label="تفاصيل المهمة"
+                type="text"
+                fullWidth
+                value={editDetails}
+                onChange={(e) => setEditDetails(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => setOpenEditDialog(false)} color="primary">إلغاء</Button>
+                <Button
+                onClick={() => {
+                    if (editTitle.trim() === "" || editDetails.trim() === "") {
+                        alert("الرجاء ملء جميع الحقول");
+                        return;
+                    }
+                    onEdit(editTitle, editDetails);
+                    setOpenEditDialog(false);
+                }}
+                color="primary"
+                >
+                حفظ
+                </Button>
+            </DialogActions>
+            </Dialog>
+        {/*=== END EDIT DIALO === */}
 
         </>
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,18 +8,107 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-
-
+import { useState } from 'react';
 
 //COMPONENTS
 import Todo from './Todo';
 import { Grid } from '@mui/material';
 
+//OTHERS
+import { v4 as uuidv4 } from 'uuid';
+
+
+const initialTodos =[
+    {
+        id : uuidv4(),
+        title:"قراءة كتاب",
+        details:"قراءة كتاب وروايه 47 صفحه",
+        isCompleted : false,
+
+    },
+    {
+        id : uuidv4(),
+        title:"الذهاب للتمرين",
+        details:" انزل التمرين الساعه 2",
+        isCompleted : false,
+
+    },
+    {
+        id : uuidv4(),
+        title:"قراءة كتاب",
+        details:"قراءة كتاب وروايه 47 صفحه",
+        isCompleted : false,
+
+    },
+]
+
 export default function TodoList() {
+
+
+
+    const[todos , setTodos] = useState(initialTodos);
+    const[titleInput , setTitleInput] = useState("");
+
+
+    const todosJsx = todos.map((t)=> {
+        return <Todo
+         key = {t.id}
+         title={t.title}
+         details = {t.details}
+         isCompleted = {t.isCompleted}
+         onComplete={() => handelCompleteTodo(t.id)}
+         onDelete={() => handleDeleteTodo(t.id)}
+         onEdit={(newTitle, newDetails) => handleEditTodo(t.id, newTitle , newDetails)}
+         />
+    })
+
+
+    function handleEditTodo(id, newTitle, newDetails) {
+        const updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                return {...todo, title: newTitle, details: newDetails};
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+
+    function handleDeleteTodo(id) {
+        const updatedTodos = todos.filter(todo => todo.id !== id);
+        setTodos(updatedTodos);
+    }
+
+    function handelCompleteTodo(id){
+        const updateTodos = todos.map((todo) => {
+            if(todo.id === id) {
+                return {...todo, isCompleted: !todo.isCompleted};
+            }
+            return todo;
+        });
+
+        setTodos(updateTodos);
+    }
+    function handelAddClick()
+    {
+        if (!titleInput.trim()) return;
+        const newTodo = {
+
+            id : uuidv4(),
+            title: titleInput,
+            details:"",
+            isCompleted : false,
+        }
+        setTodos ([...todos , newTodo]);
+        setTitleInput("");
+
+    }
   return (
 
       <Container maxWidth="sm" style={{textAlign:"center"}}>
-        <Card sx={{ minWidth: 275 }}>
+        <Card sx={{ minWidth: 275 }} style={{
+            maxHeight :"90vh",
+            overflow:"scroll",
+        }}>
             <CardContent>
                 <Typography variant = "h3" style={{fontWeight:"bold"}}> مهامي </Typography>
 
@@ -41,7 +130,7 @@ export default function TodoList() {
                 {/* === FILLTER BUTTONS === */}
 
                 {/*ALL TODOS */}
-                <Todo/>
+                {todosJsx}
                 {/* === ALLTODOS === */}
 
                 {/*INPUT + ADD BUTTON*/}
@@ -55,14 +144,25 @@ export default function TodoList() {
                         style={{width:"100%"}}
                         id="outlined-basic"
                         label="عنوان المهمة"
-                        variant="outlined" />
+                        variant="outlined"
+                        value={titleInput}
+                        onChange={(e) =>
+                        {
+                            setTitleInput(e.target.value)
+                        }
+                        }
+                        />
                     </Grid>
 
                     <Grid
                     size={4}
                     display="flex" justifyContent="space-around" alignItems="center"
                     >
-                        <Button variant="contained" style={{width:"100%" , height:"100%"}}>اضافة</Button>
+                        <Button variant="contained" style={{width:"100%" , height:"100%"}}
+                        onClick={() => {
+                            handelAddClick();
+                        }}
+                        >اضافة</Button>
                     </Grid>
 
                 </Grid>
@@ -70,6 +170,7 @@ export default function TodoList() {
 
             </CardContent>
         </Card>
+       
       </Container>
 
   );
